5ff805543cb8a0f32bb72d06f5a5584ae180cf97
Philip <96379608+philipyourdad@users.noreply.github.com>
2022-12-23 21:54:08 +0800

    Update portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -254,16 +254,7 @@
 
 })()
 
-//darkmode
-$("switch").click(function(){
-    $("body").toggleClass("dark-mode");
-    if($("body").hasClass("dark-mode")) {
-       
-    $(".switch").attr("name", "sunny-outline");
-    } else{
-    $(".switch").attr("name", "moon-outline");
-    }
-});
+
 
 
 
